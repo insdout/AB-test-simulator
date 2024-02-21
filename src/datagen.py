402df481df6bfe5ab7a7356b3a216388dec8333b
insdout <insdxout@gmail.com>
2024-02-21 22:28:33 +0200

    updates

--- src/datagen.py
+++ src/datagen.py
@@ -1,51 +1,100 @@
 import numpy as np
 import scipy.stats as stats
+import pandas as pd
+import matplotlib.pyplot as plt
+import matplotlib.dates as mdates
 
 
 class ABTestGenerator:
-    def __init__(self, base_ctr: float, uplift: float, beta: float):
+    def __init__(self, base_ctr: float, uplift: float, beta: float, skew: float, traffic_per_day: int):
         self.base_ctr = base_ctr
         self.uplift = uplift
         self.beta = beta
+        self.skew = skew
+        self.traffic_per_day = traffic_per_day
 
-    def generate_single_experiment(self, num_users: int) -> tuple[np.ndarray]:
+    def generate_single_experiment(self, num_users: int) -> tuple:
         alpha_0 = self.base_ctr * self.beta / (1 - self.base_ctr)
         alpha_1 = (self.base_ctr + self.uplift) * self.beta / (1 - self.base_ctr - self.uplift)
+
+        views_0 = np.exp(stats.norm(1, self.skew).rvs(num_users)).astype(np.int64) + 1
+        views_1 = np.exp(stats.norm(1, self.skew).rvs(num_users)).astype(np.int64) + 1
+
+        # Generate random dates within a date range
+        days_needed = int(np.ceil(num_users / self.traffic_per_day))
+        start_date = pd.to_datetime('2024-01-01')
+        end_date = start_date + pd.Timedelta(days=days_needed)
+        days = np.random.choice(pd.date_range(start=start_date, end=end_date), size=num_users, replace=True)
 
         ctrs_0 = stats.beta.rvs(a=alpha_0, b=self.beta, size=num_users)
         ctrs_1 = stats.beta.rvs(a=alpha_1, b=self.beta, size=num_users)
 
-        clicks_0 = stats.binom(n=1, p=ctrs_0).rvs(size=num_users)
-        clicks_1 = stats.binom(n=1, p=ctrs_1).rvs(size=num_users)
+        clicks_0 = stats.binom(n=views_0, p=ctrs_0).rvs()
+        clicks_1 = stats.binom(n=views_1, p=ctrs_1).rvs()
 
-        return ctrs_0, ctrs_1, clicks_0, clicks_1
+        return {
+            'days': days,
+            'ctrs_0': ctrs_0,
+            'ctrs_1': ctrs_1,
+            'clicks_0': clicks_0,
+            'clicks_1': clicks_1,
+            'views_0': views_0,
+            'views_1': views_1
+        }
 
-    def generate_n_experiment(
-            self,
-            num_users: int,
-            n_runs: int
-    ) -> tuple[list[np.ndarray]]:
-
-        ctrs_0_list, ctrs_1_list, clicks_0_list, clicks_1_list = [], [], [], []
+    def generate_n_experiment(self, num_users: int, n_runs: int) -> tuple:
+        experiments = {
+            'days': [],
+            'ctrs_0': [],
+            'ctrs_1': [],
+            'clicks_0': [],
+            'clicks_1': [],
+            'views_0': [],
+            'views_1': []
+        }
 
         for _ in range(n_runs):
-            ctrs_0, ctrs_1, clicks_0, clicks_1 = self.generate_single_experiment(num_users)
-            ctrs_0_list.append(ctrs_0)
-            ctrs_1_list.append(ctrs_1)
-            clicks_0_list.append(clicks_0)
-            clicks_1_list.append(clicks_1)
+            experiment = self.generate_single_experiment(num_users)
+            for key, value in experiment.items():
+                experiments[key].append(value)
 
-        return ctrs_0_list, ctrs_1_list, clicks_0_list, clicks_1_list
+        return experiments
+
+
+def plot_mean_ctr_per_day(days_list, ctrs_0, ctrs_1):
+
+    ctr_df = pd.DataFrame({'Date': days_list, 'CTR_0': ctrs_0, 'CTR_1': ctrs_1})
+    mean_ctr_per_day = ctr_df.groupby('Date').mean()
+
+    plt.plot(mean_ctr_per_day.index, mean_ctr_per_day['CTR_0'], label='CTR_0')
+    plt.plot(mean_ctr_per_day.index, mean_ctr_per_day['CTR_1'], label='CTR_1')
+    plt.xlabel('Date')
+    plt.ylabel('Mean CTR')
+    plt.title('Mean CTR per Day')
+    # Format x-axis labels to show only day and month
+    plt.gca().xaxis.set_major_formatter(mdates.DateFormatter('%d/%m'))
+    # Rotate x-axis labels by 45 degrees
+    plt.xticks(rotation=45)
+    plt.legend()
+    plt.tight_layout()
+    plt.show()
 
 
 if __name__ == '__main__':
-    # Example usage:
-    base_ctr = 0.1
-    uplift = 0.02
-    beta = 2.0
-    num_users = 10000
-    n_runs = 5
+    base_ctr = 0.02
+    uplift = 0.01
+    beta = 250
+    skew = 0.4
+    traffic_per_day = 100
+    num_users = 6000
+    n_runs = 1
 
-    ab_test_generator = ABTestGenerator(base_ctr, uplift, beta)
-    ctrs_0_list, ctrs_1_list, clicks_0_list, clicks_1_list = ab_test_generator.generate_n_experiment(num_users, n_runs)
-    print(np.mean(ctrs_0_list), np.mean(clicks_1_list))
+    ab_test_generator = ABTestGenerator(base_ctr, uplift, beta, skew, traffic_per_day)
+    experiments = ab_test_generator.generate_n_experiment(num_users, n_runs)
+
+    # Accessing the data from the experiments dictionary
+    for i in range(n_runs):
+        days, ctrs_0, ctrs_1, clicks_0, clicks_1, views_0, views_1 = experiments['days'][i], experiments['ctrs_0'][i], experiments['ctrs_1'][i], experiments['clicks_0'][i], experiments['clicks_1'][i], experiments['views_0'][i], experiments['views_1'][i]
+        print(np.mean(ctrs_0), np.mean(ctrs_1), len(ctrs_0), sum(views_0), sum(views_1))
+        print(np.sum(clicks_0)/np.sum(views_0), np.sum(clicks_1)/np.sum(views_1))
+        plot_mean_ctr_per_day(days, ctrs_0, ctrs_1)
--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,7 @@
 import streamlit as st
 from src.testdesign import design_binomial_experiment
+from src.datagen import ABTestGenerator
+import numpy as np
 
 
 def main():
@@ -15,7 +17,7 @@ def main():
     st.sidebar.title('Data Generation Model:')
     st.sidebar.latex(r'''
     \begin{align*}
-    &\text{Views} \sim \text{LogNormal}(1, \sigma) \\
+    &\text{Views} \sim \text{LogNormal}(1, \text{skew}) \\
     &\text{CTR} \sim \text{Beta}(\alpha, \beta) \\
     &\text{Clicks} \sim \text{Binomial}(\text{Views}, \text{CTR})        
     \end{align*}
@@ -24,9 +26,9 @@ def main():
     with st.sidebar.form(key='Data Generation Model'):
         base_ctr_pcnt = st.slider('Base CTR, %', min_value=0.1, max_value=20.0, step=0.1, value=2.0)
         uplift_pcnt = st.slider('CTR Uplift, %', min_value=0.1, max_value=40.0, step=0.1, value=1.0)
-        views_sigma = st.slider('Sigma', min_value=1, max_value=500, step=1, value=200)
-        ctr_beta = st.slider('Beta', min_value=1, max_value=500, step=1, value=200)
-        sb_submit_button = st.form_submit_button(label='Submit')
+        skew = st.slider('Skew', min_value=0.1, max_value=4.0, step=0.1, value=2.0)
+        ctr_beta = st.slider('Beta', min_value=1, max_value=500, step=1, value=50)
+        sb_submit_button = st.form_submit_button(label='Apply')
 
     st.title('APP')
 
@@ -34,16 +36,23 @@ def main():
     with st.form(key='Experiment Design'):
         col1, col2, col3 = st.columns(3)
         alpha = col1.slider(r'$\alpha$,  Type I Error:', min_value=0.01, max_value=0.2, step=0.01, value=0.05)
+        n_samples = col1.slider(r'n_samples to estimate $\overline{\text{CTR}}|H_0$', min_value=100, max_value=10000, step=1, value=1000)
         beta = col2.slider(r'$\beta$, Type II Error:', min_value=0.01, max_value=0.8, step=0.01, value=0.2)
         mde = col3.slider('Minimum Detectable Effect', min_value=0.1, max_value=40.0, step=0.1, value=1.0)
         ed_submit = st.form_submit_button(label='Estimate')
 
     uplift = uplift_pcnt/100
-    h0_ctr = base_ctr_pcnt/100
+    base_ctr = base_ctr_pcnt/100
     mde = mde/100
-    if ed_submit:
-        n = design_binomial_experiment(min_detectable_change=mde, p_0=h0_ctr, alpha=alpha, beta=beta)
-        st.write(f'p_0: {h0_ctr} uplift: {uplift} alpha: {alpha} beta: {beta} N: {n}')
+
+    st.write(base_ctr, uplift, ctr_beta, skew)
+    datagen = ABTestGenerator(base_ctr, uplift, ctr_beta, 200.0, traffic_per_day=1000) # TODO: Think about traffic per day!
+    days, ctrs_0, ctrs_1, clicks_0, clicks_1, views_0, views_1 = datagen.generate_single_experiment(n_samples)
+
+    estimated_ctr_h0 = np.sum(clicks_0)/np.sum(views_0)
+    st.write(estimated_ctr_h0, mde, alpha, skew)
+    n = design_binomial_experiment(min_detectable_change=mde, p_0=estimated_ctr_h0, alpha=alpha, beta=beta)
+    st.write(f'Estimated CTR: {np.round(estimated_ctr_h0, 5)} \nMinimal number of views for test: {n}')
 
 
 if __name__ == '__main__':
